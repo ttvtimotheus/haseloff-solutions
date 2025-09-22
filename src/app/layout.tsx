79c378430bada31e40518bfdbb72c299581b7fb8
ttvtimotheus <haseloff.timo@gmail.com>
2025-09-22 19:46:07 +0200

    feat: add Vercel Analytics to root layout and sort package.json dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react';
+import { Analytics } from "@vercel/analytics/next"
 
 type Props = {
   children: ReactNode;
@@ -7,5 +8,10 @@ type Props = {
 // Since we have a `not-found.tsx` page at the root level, we need to have
 // a root layout as well. Otherwise, you'll get an error.
 export default function RootLayout({ children }: Props) {
-  return children;
+  return (
+    <>
+      {children}
+      <Analytics />
+    </>
+  );
 }
